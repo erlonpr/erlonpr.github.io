c4ff914c8d239290c03a5f1ab8bc8ee5e5c7950e
Erlon <erlonpr@gmail.com>
2016-09-03 15:47:37 +0000

    interactive menu with javascript

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,23 @@
 var main = function() {
 
-  $('.description').hide();
+    $('.description').hide();
+    $('.sub-description').hide();
+    
   
-   $('.item').click(function() {
-     $(this).children('.description').toggle();
-     $(this).addClass('reading');
-   });
+    $('.item').click(function() {
+        
+        $('.description').hide(); 
+        $(this).children('.description').show();
+        
+        $('.item').removeClass('reading');
+        $(this).addClass('reading');
+   
+    });
+    
+    $('.sub-item').click(function() {
+        $('.sub-description').hide();
+        $(this).children('.sub-description').show();
+    });
 };
 
 $(document).ready(main);
