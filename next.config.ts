dbc8566652be12405329eb747372e1666f363454
erlonpr <erlonpr@gmail.com>
2026-04-16 19:32:03 -0300

    build(config): configure static export for GitHub Pages
    Update 'next.config.ts' to enable static site generation (SSG) for deployment.
    
    Changes introduced:
    - Set 'output: "export"' to generate a fully static HTML/CSS/JS build.
    - Set 'images.unoptimized: true' to disable Next.js built-in image optimization, since the Image Optimization API is not supported in static exports like GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: 'export', // Generate static files (HTML, CSS, JS)
+  images: {
+    unoptimized: true, // Disable image optimization (required for GitHub Pages static export)
+  },
+
 };
 
 export default nextConfig;
